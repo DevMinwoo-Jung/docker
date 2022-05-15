5fb23dacae2e3ccb32e5527e6fa2ea6695584088
DEVMINWOO-JUNG <applejung312@gmail.com>
2022-05-15 16:52:39 +0900

    20220515

--- nodeJs/server.js
+++ nodeJs/server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0'; // 호스트 지정
 
 const app = express(); // express 어플 생성
 app.get('/', (req, res) => {
-    res.send('Hello World!'); // "/" 이 경로로 요청이 오면 hello world를 결과값으로 전달달});
+    res.send('야야야야바뀜ddd??'); // "/" 이 경로로 요청이 오면 hello world를 결과값으로 전달달});
 });
 app.listen(PORT, HOST); // 해당 포트와 호스트에서 http 서버를 시작
 console.log(`Running on http://${HOST}: ${PORT}`);
